40ec4cfbd6a06e3c07fea1c38e4554ef390e14cc
Gieted <gieted@proton.me>
2022-12-26 20:23:50 +0100

    Finish progress bar

--- lib/widgets/player/progress_bar.dart
+++ lib/widgets/player/progress_bar.dart
@@ -1,6 +1,8 @@
 import 'package:dreamscenter/util.dart';
 import 'package:flutter/widgets.dart';
 
+import '../../colors.dart';
+
 class ProgressBar extends StatelessWidget {
   final double progress;
 
@@ -11,19 +13,28 @@ class ProgressBar extends StatelessWidget {
     return LayoutBuilder(builder: (context, constraints) {
       return Stack(children: [
         Container(
-          key: const Key('background'),
-          decoration: BoxDecoration(
-            color: const Color(0xFF333333),
-            borderRadius: BorderRadius.circular(longestSize(context)),
-          ),
-        ),
-        Container(
-            key: const Key('media progress'),
-            width: constraints.maxWidth * progress,
+            key: const Key('background'),
             decoration: BoxDecoration(
-              color: const Color(0xFFFF5C00),
+              color: const Color(0xFF333333),
               borderRadius: BorderRadius.circular(longestSize(context)),
             )),
+        ClipRRect(
+            borderRadius: BorderRadius.circular(longestSize(context)),
+            child: Transform.translate(
+                offset: Offset(-constraints.maxWidth, 0),
+                child: OverflowBox(
+                    alignment: Alignment.centerLeft,
+                    maxWidth: constraints.maxWidth * 2,
+                    child: Container(
+                        key: const Key('media progress'),
+                        height: constraints.maxHeight,
+                        width: constraints.maxWidth * progress +
+                            constraints.maxWidth,
+                        decoration: BoxDecoration(
+                          color: Colors.primaryDark,
+                          borderRadius:
+                              BorderRadius.circular(longestSize(context)),
+                        ))))),
       ]);
     });
   }
