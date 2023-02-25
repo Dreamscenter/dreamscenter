8004e4f3259c3c39a77fe0585c0f09321cfb4222
Gieted <gieted@proton.me>
2023-02-25 12:25:41 +0100

    Fix fullscreen on Firefox

--- lib/player/fullscreen/fullscreen_web.dart
+++ lib/player/fullscreen/fullscreen_web.dart
@@ -1,11 +1,22 @@
 import 'dart:html';
 
+import 'package:js/js.dart';
+
 Future<void> enterFullscreen() async {
   document.documentElement!.requestFullscreen();
 }
 
+@JS()
+@staticInterop
+class JSDocument {}
+
+extension JSDocumentExtension on JSDocument {
+  external void exitFullscreen();
+}
+
 Future<void> exitFullscreen() async {
-  document.exitFullscreen();
+  final jsDocument = document as JSDocument;
+  jsDocument.exitFullscreen();
 }
 
 Future<bool> isFullscreen() async {
